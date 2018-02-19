ae0e468902292e0d807938b7a5222510ad7b9b7b
Jamie Gaehring <gaehj457@gmail.com>
2018-02-19 10:38:23 -0500

    pass in login as a component instead of a plugin, to fix router issue.

--- src/spa/loader.js
+++ src/spa/loader.js
@@ -7,11 +7,22 @@ import router from './router';
 Vue.config.productionTip = false;
 
 export default (login) => {
-  Vue.use(login)
+  // If passed a login component, register it on the global Vue instance,
+  // then add it to routes and the components object
+  let components = { App }
+  if (typeof login !== 'undefined') {
+    const LoginComponent = Vue.component(login.name, login);
+    router.addRoutes([{
+      path: '/login',
+      name: 'Login',
+      component: LoginComponent
+    }]);
+    components.LoginComponent = LoginComponent;
+  }
   return new Vue({
     el: '#app',
     router,
-    components: { App },
+    components: components,
     template: '<App/>'
   })
 };
