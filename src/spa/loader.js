550cc7a70694f82a2106b452614e005803ed997b
Jamie Gaehring <gaehj457@gmail.com>
2018-02-20 06:27:46 -0500

    change store name

--- src/spa/loader.js
+++ src/spa/loader.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import router from './router';
-import store from './store/store'
+import store from './store'
 import App from './App';
 
 Vue.config.productionTip = false;
