a0ddf2064734ab7fb3be2553c6a414b73394bec8
Jamie Gaehring <gaehj457@gmail.com>
2018-06-15 16:14:56 -0400

    delete some unneeded devServer config properties

--- config/index.js
+++ config/index.js
@@ -12,14 +12,6 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
 
-    // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
-    errorOverlay: true,
-    notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
