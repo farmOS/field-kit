b5dd15a10de4bd83e2f35f789ab7d201c732d544
Jamie Gaehring <gaehj457@gmail.com>
2018-06-26 19:39:52 -0400

    comment out some old native config till I know what to do with it

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,86 +55,98 @@ const prodWebpackConfig = merge(baseWebpackConfig, {
   ]
 })
 
-// This is currently just for reference, and is not being used
-// TODO: Migrate this to the native repo
-const nativeWebpackConfig = merge(prodWebpackConfig, {
-  output: {
-    path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
-  },
-  plugins: [
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    // new HtmlWebpackPlugin({
-    //   filename: process.env.NODE_ENV === 'testing'
-    //     ? 'index.html'
-    //     : config.build.index,
-    //   template: 'index.html',
-    //   inject: true,
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeAttributeQuotes: true
-    //     // more options:
-    //     // https://github.com/kangax/html-minifier#options-quick-reference
-    //   },
-    //   // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-    //   chunksSortMode: 'dependency'
-    // }),
-    // extract css into its own file
-    new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // Setting the following option to `false` will not extract CSS from codesplit chunks.
-      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
-      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
-    }),
-    // Compress extracted CSS. We are using this plugin so that possible
-    // duplicated CSS from different components can be deduped.
-    new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap
-        ? { safe: true, map: { inline: false } }
-        : { safe: true }
-    }),
-    // keep module.id stable when vendor modules does not change
-    new webpack.HashedModuleIdsPlugin(),
-    // enable scope hoisting
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity
-    }),
-    // This instance extracts shared chunks from code splitted chunks and bundles them
-    // in a separate chunk, similar to the vendor chunk
-    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3
-    }),
-
-  ]
-})
+/**
+  * This is currently just for reference, and not being used.
+  * These are the parts of the old native configuration that
+  * were separated out when the client was originally made
+  * into its own library, but they could be useful later,
+  * either as a baseline config for the native repo,
+  * or if we want to figure out a better way to separate
+  * static assets as a part of the client's build process.
+  * If this config is entirely removed, make sure to also
+  * remove the ExtractTextPlugin and OptimizeCSSPlugin from
+  * the devDependencies, since they're not otherwise used.
+  * Note that HtmlWebpackPlugin is no longer installed, but
+  * probably shouldn't be used since the client has no index.html
+**/
+// const nativeWebpackConfig = merge(prodWebpackConfig, {
+//   output: {
+//     path: config.build.assetsRoot,
+//     filename: utils.assetsPath('js/[name].[chunkhash].js'),
+//     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+//   },
+//   plugins: [
+//     // generate dist index.html with correct asset hash for caching.
+//     // you can customize output by editing /index.html
+//     // see https://github.com/ampedandwired/html-webpack-plugin
+//     new HtmlWebpackPlugin({
+//       filename: process.env.NODE_ENV === 'testing'
+//         ? 'index.html'
+//         : config.build.index,
+//       template: 'index.html',
+//       inject: true,
+//       minify: {
+//         removeComments: true,
+//         collapseWhitespace: true,
+//         removeAttributeQuotes: true
+//         // more options:
+//         // https://github.com/kangax/html-minifier#options-quick-reference
+//       },
+//       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+//       chunksSortMode: 'dependency'
+//     }),
+//     // extract css into its own file
+//     new ExtractTextPlugin({
+//       filename: utils.assetsPath('css/[name].[contenthash].css'),
+//       // Setting the following option to `false` will not extract CSS from codesplit chunks.
+//       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
+//       // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
+//       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
+//       allChunks: true,
+//     }),
+//     // Compress extracted CSS. We are using this plugin so that possible
+//     // duplicated CSS from different components can be deduped.
+//     new OptimizeCSSPlugin({
+//       cssProcessorOptions: config.build.productionSourceMap
+//         ? { safe: true, map: { inline: false } }
+//         : { safe: true }
+//     }),
+//     // keep module.id stable when vendor modules does not change
+//     new webpack.HashedModuleIdsPlugin(),
+//     // enable scope hoisting
+//     new webpack.optimize.ModuleConcatenationPlugin(),
+//     // split vendor js into its own file
+//     new webpack.optimize.CommonsChunkPlugin({
+//       name: 'vendor',
+//       minChunks (module) {
+//         // any required modules inside node_modules are extracted to vendor
+//         return (
+//           module.resource &&
+//           /\.js$/.test(module.resource) &&
+//           module.resource.indexOf(
+//             path.join(__dirname, '../node_modules')
+//           ) === 0
+//         )
+//       }
+//     }),
+//     // extract webpack runtime and module manifest to its own file in order to
+//     // prevent vendor hash from being updated whenever app bundle is updated
+//     new webpack.optimize.CommonsChunkPlugin({
+//       name: 'manifest',
+//       minChunks: Infinity
+//     }),
+//     // This instance extracts shared chunks from code splitted chunks and bundles them
+//     // in a separate chunk, similar to the vendor chunk
+//     // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
+//     new webpack.optimize.CommonsChunkPlugin({
+//       name: 'app',
+//       async: 'vendor-async',
+//       children: true,
+//       minChunks: 3
+//     }),
+//
+//   ]
+// })
 
 if (config.build.productionGzip) {
   const CompressionWebpackPlugin = require('compression-webpack-plugin')
