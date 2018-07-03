b90bfed51a012e7c4ea473c4852f43b1dd9569b3
Jamie Gaehring <gaehj457@gmail.com>
2018-07-03 16:39:01 -0400

    switch eslint from Standard JS to AirBNB Style Guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,9 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'plugin:vue/essential',
+    // https://github.com/airbnb/javascript/blob/master/README.md
+    'airbnb-base'
   ],
   // required to lint *.vue files
   plugins: [
