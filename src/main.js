98d1f8b16e3b17dab5c042688e26d7db49617eda
Jamie Gaehring <gaehj457@gmail.com>
2018-07-20 13:01:15 -0400

    lint main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import loader from 'farmos-client';
+import app from 'farmos-client';
 import data from '@/data';
 import login from '@/login';
-import jQuery from './jquery';
-const app = loader(data, login);
+
+app(data, login);
