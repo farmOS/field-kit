6b131000e0146d9344f3d2b4b1148a6049ad4da1
Jamie Gaehring <gaehj457@gmail.com>
2018-07-20 20:54:21 -0400

    fix: jQuery still needs to be imported from main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import app from 'farmos-client';
 import data from '@/data';
 import login from '@/login';
+// TODO: remove jQuery or pull it in via npm
+import jQuery from './jquery'; // eslint-disable-line no-unused-vars
 
 app(data, login);
