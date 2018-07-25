ee2d8c86240cd833e2e76d8c082935e434aec21d
Jamie Gaehring <gaehj457@gmail.com>
2018-07-25 13:30:06 -0400

    lint data/index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,16 @@
 import dataModule from './dataModule';
 
 export default {
-  install(Vue, {store, router}) {
+  install(Vue, { store, router }) {
     store.registerModule('data', dataModule);
     // TODO: give the client more control over when to retrieve cached logs
-    router.afterEach( (to, from) => {
+    router.afterEach((to) => {
       if (to.name === 'Observations') {
         store.commit('clearLogs');
         store.dispatch('loadCachedLogs', 'farm_observation');
       }
     });
-    store.subscribe( (mutation, state) => {
+    store.subscribe((mutation) => {
       if (mutation.type === 'addLogAndMakeCurrent') {
         store.dispatch('createRecord', mutation.payload);
       }
@@ -18,5 +18,5 @@ export default {
         store.dispatch('updateRecord', mutation.payload);
       }
     });
-  }
-}
+  },
+};
