7104d8c56b352bbaf4389160a8574b5b246d505e
Jamie Gaehring <gaehj457@gmail.com>
2018-08-02 18:35:15 -0400

    remove router hook

--- src/data/index.js
+++ src/data/index.js
@@ -3,18 +3,6 @@ import dataModule from './dataModule';
 export default {
   install(Vue, { store /* , router */ }) {
     store.registerModule('data', dataModule);
-    /*
-      TODO: Delete this when we're sure we want to give over full control
-      to the client components when logs need to be loaded from
-      local persistance, whether that's WebSQL, IDB, etc. Leaving it for
-      reference temporarily.
-    */
-    // router.afterEach((to) => {
-    //   if (to.name === 'Observations') {
-    //     store.commit('clearLogs');
-    //     store.dispatch('loadCachedLogs', 'farm_observation');
-    //   }
-    // });
     store.subscribe((mutation) => {
       if (mutation.type === 'addLogAndMakeCurrent') {
         store.dispatch('createRecord', mutation.payload);
