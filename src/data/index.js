024243151aef9a37a5c6c5ba7e2b2606c9b632b1
Jamie Gaehring <gaehj457@gmail.com>
2018-08-03 12:20:53 -0400

    dispatch `pushToServer` from data plugin, via store.subscribe()

--- src/data/index.js
+++ src/data/index.js
@@ -10,6 +10,11 @@ export default {
       if (mutation.type === 'updateCurrentLog' && !mutation.payload.isCachedLocally) {
         store.dispatch('updateRecord', mutation.payload);
       }
+      if (mutation.type === 'updateAllLogs') {
+        // TODO: Uncomment this once `pushToServer` can handle multiple logs
+        // const payload = store.logs.filter(log => log.isReadyToSync);
+        store.dispatch('pushToServer'/* , payload */);
+      }
     });
   },
 };
