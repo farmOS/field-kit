8e84112f423a89dfc2d7eb9705f3bd18023f0152
Jamie Gaehring <gaehj457@gmail.com>
2018-08-22 15:47:31 -0400

    finish linting login plugin

--- src/login/index.js
+++ src/login/index.js
@@ -1,16 +1,16 @@
-import Login from './Login.vue';
+import Login from './Login.vue'; // eslint-disable-line import/extensions
 import loginModule from './loginModule';
-import mixinLogic from './mixin.js';
+import mixinLogic from './mixin';
 
 export default {
-  install( Vue, { router, store }) {
-    const LoginComponent = Vue.component(Login.name, Login)
+  install(Vue, { router, store }) {
+    const LoginComponent = Vue.component(Login.name, Login);
     router.addRoutes([{
       path: '/login',
       name: 'Login',
-      component: LoginComponent
+      component: LoginComponent,
     }]);
     store.registerModule('user', loginModule);
-    Vue.mixin(mixinLogic)
-  }
+    Vue.mixin(mixinLogic);
+  },
 };
--- src/login/mixin.js
+++ src/login/mixin.js
@@ -1,10 +1,10 @@
 export default {
-  created: function () {
+  created() {
     if (this.$store.state.user.isLoggedIn) {
-      this.$store.commit('changeGreeting', `Welcome ${this.$store.state.user.name}!`)
-      return
-    };
+      this.$store.commit('changeGreeting', `Welcome ${this.$store.state.user.name}!`);
+      return;
+    }
     this.$store.commit('changeGreeting', 'Please enter your farmOS credentials');
-    this.$router.push({path: 'login'});
-  }
+    this.$router.push({ path: 'login' });
+  },
 };
