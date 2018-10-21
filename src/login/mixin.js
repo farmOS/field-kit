c3cf2954eff6385e128ac666517c4ba2e3ff2374
Jamie Gaehring <gaehj457@gmail.com>
2018-10-21 19:35:52 -0400

    remove greeting

--- src/login/mixin.js
+++ src/login/mixin.js
@@ -1,10 +1,8 @@
 export default {
   created() {
     if (this.$store.state.user.isLoggedIn) {
-      this.$store.commit('changeGreeting', `Welcome ${this.$store.state.user.name}!`);
       return;
     }
-    this.$store.commit('changeGreeting', 'Please enter your farmOS credentials');
     this.$router.push({ path: 'login' });
   },
 };
