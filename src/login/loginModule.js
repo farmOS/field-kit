a047a48b42636ebe252eb602d8d56d879a86031b
Jamie Gaehring <gaehj457@gmail.com>
2018-12-03 12:41:34 -0500

    revert to fetch() in checkUser() and requestToken() functions

--- src/login/loginModule.js
+++ src/login/loginModule.js
@@ -138,42 +138,25 @@ function checkUser(url) {
       Accept: 'json',
     };
 
-    // fetch(userUrl, {
-    //   method: 'GET',
-    //   headers: requestHeaders,
-    //   credentials: 'include',
-    // }).then((response) => {
-    //   console.log('fetch response: ', response);
-    //   if (!response.ok) {
-    //     throw response;
-    //   }
-    //   return response.json();
-    // }).then((response) => {
-    //   console.log('USER REQUEST SUCCESS!!');
-    //   console.log(`STATUS: ${response.status}`);
-    //   console.log(`STATUS TEXT: ${response.statusText}`);
-    //   resolve(response);
-    // }).catch((error) => {
-    //   console.log('USER REQUEST ERROR...');
-    //   console.log(`USER RESPONSE: ${JSON.stringify(error)}`);
-    //   reject(error);
-    // });
-
-    $.ajax({ // eslint-disable-line no-undef
-      type: 'GET',
-      url: userUrl,
+    fetch(userUrl, {
+      method: 'GET',
       headers: requestHeaders,
-      success: (response) => {
-        console.log('USER REQUEST SUCCESS!!');
-        console.log(`STATUS: ${response.status}`);
-        console.log(`STATUS TEXT: ${response.statusText}`);
-        resolve(response);
-      },
-      error: (error) => {
-        console.log('USER REQUEST ERROR...');
-        console.log(`USER RESPONSE: ${JSON.stringify(error)}`);
-        reject(error);
-      },
+      credentials: 'include',
+    }).then((response) => {
+      console.log('fetch response: ', response);
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    }).then((response) => {
+      console.log('USER REQUEST SUCCESS!!');
+      console.log(`STATUS: ${response.status}`);
+      console.log(`STATUS TEXT: ${response.statusText}`);
+      resolve(response);
+    }).catch((error) => {
+      console.log('USER REQUEST ERROR...');
+      console.log(`USER RESPONSE: ${JSON.stringify(error)}`);
+      reject(error);
     });
   });
   return submissionPromise;
@@ -256,40 +239,24 @@ function requestToken(url) {
       Accept: 'json',
     };
 
-    // fetch(tokenUrl, {
-    //   method: 'GET',
-    //   headers: requestHeaders,
-    //   credentials: 'include',
-    // }).then((response) => {
-    //   console.log('fetch response: ', response);
-    //   if (!response.ok) {
-    //     throw response;
-    //   }
-    //   return response.text();
-    // }).then((response) => {
-    //   console.log(`TOKEN OBTAINED: ${response}`);
-    //   // const storage = window.localStorage;
-    //   resolve(response);
-    // }).catch((error) => {
-    //   console.log('TOKEN ERROR: NO TOKEN OBTAINED');
-    //   console.log(`RESPONSE: ${JSON.stringify(error)}`);
-    //   reject(error);
-    // });
-
-    $.ajax({ // eslint-disable-line no-undef
-      type: 'GET',
-      url: tokenUrl,
+    fetch(tokenUrl, {
+      method: 'GET',
       headers: requestHeaders,
-      success: (response) => {
-        console.log(`TOKEN OBTAINED: ${response}`);
-        // const storage = window.localStorage;
-        resolve(response);
-      },
-      error: (error) => {
-        console.log('TOKEN ERROR: NO TOKEN OBTAINED');
-        console.log(`RESPONSE: ${JSON.stringify(error)}`);
-        reject(error);
-      },
+      credentials: 'include',
+    }).then((response) => {
+      console.log('fetch response: ', response);
+      if (!response.ok) {
+        throw response;
+      }
+      return response.text();
+    }).then((response) => {
+      console.log(`TOKEN OBTAINED: ${response}`);
+      // const storage = window.localStorage;
+      resolve(response);
+    }).catch((error) => {
+      console.log('TOKEN ERROR: NO TOKEN OBTAINED');
+      console.log(`RESPONSE: ${JSON.stringify(error)}`);
+      reject(error);
     });
   });
   return submissionPromise;
