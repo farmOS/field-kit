48a9a47f1c5eb8e8c26f3b2d0af0cb42651d8cf1
Jamie Gaehring <gaehj457@gmail.com>
2019-01-07 20:42:39 -0500

    eslint-disable-line (import/extensions)

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 import Vue from 'vue';
 import router from './router';
 import store from './store';
-import App from './App.vue';
+import App from './App.vue'; // eslint-disable-line import/extensions
 import './bootstrap-simplex.min.css';
 
 Vue.config.productionTip = false;
