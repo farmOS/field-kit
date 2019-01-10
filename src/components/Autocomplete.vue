b3b61ee0f731fa327a3519ccfe9d571a3b8ec9f7
Jamie Gaehring <gaehj457@gmail.com>
2019-01-10 11:56:47 -0500

    tweak autocomplete styles & behavior

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -3,19 +3,22 @@
     <div class="form-item form-item-name form-group">
       <label for="search" class="control-label">Add {{ label }} to log</label>
       <input
-        @input="doSearch($event.target.value)"
+        v-model="search"
         placeholder="Enter text to search"
         type="text"
         class="form-control"
+        @focus="openResults"
+        @input="doSearch(search)"
         @keydown.down="onArrowDown"
         @keydown.up="onArrowUp"
         @keydown.enter="onEnter"
         autofocus>
       <ul
-        v-show="searchResults.length > 0"
-        class="search-results">
+        v-show="isOpen && searchResults.length > 0"
+        class="list-group search-results">
         <li
           v-for="(result, i) in searchResults"
+          class="list-group-item"
           :class="{ 'is-active': i === counter }"
           @click="selectSearchResult(searchResults[i].id)">
           {{result.name}}
@@ -47,12 +50,17 @@ export default {
   props: ['objects', 'label'],
   data() {
     return {
+      search: '',
       searchResults: [],
       selectedObjects: [],
       counter: 0,
+      isOpen: false,
     };
   },
   methods: {
+    openResults(e) {
+      this.isOpen = true;
+    },
     // The search method matches partial strings, and is case insensitive
     // Results are limited to a maximum of 10
     doSearch(val) {
@@ -76,6 +84,8 @@ export default {
         // eslint prefers strict equivalence, but I need non-strict equivalence here
         this.selectedObjects = this.selectedObjects.concat(selectedResult);
         this.$emit('results', this.selectedObjects);
+        this.search = '';
+        this.doSearch(this.search);
       }
     },
     // Remove an object when the remove button is pressed
@@ -83,30 +93,45 @@ export default {
       this.selectedObjects = this.selectedObjects.filter(results => results !== object);
       this.$emit('results', [this.selectedObjects]);
     },
-
     onArrowDown() {
       if (this.counter < (this.searchResults.length - 1)) {
         this.counter = this.counter + 1;
       }
     },
-
-    onArrowUp() {
+    onArrowUp(evt) {
+      // Use preventDefault so cursor doesn't return to the beginning
+      evt.preventDefault();
       if (this.counter > 0) {
         this.counter = this.counter - 1;
       }
     },
-
     onEnter() {
       const id = this.searchResults[this.counter].id;
       this.selectSearchResult(id);
     },
-
+    handleClickOutside(evt) {
+      if (!this.$el.contains(evt.target)) {
+        this.isOpen = false;
+        this.arrowCounter = -1;
+      }
+    },
+  },
+  mounted() {
+    document.addEventListener('click', this.handleClickOutside)
+  },
+  destroyed() {
+    document.removeEventListener('click', this.handleClickOutside)
   },
 };
 
 </script>
 
 <style scoped>
+  .search-results {
+    position: absolute;
+    z-index: 100;
+    box-shadow: 2px 2px 5px rgba(0,0,0,.5)
+  }
   .search-results li {
     list-style: none;
   }
