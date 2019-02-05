1fbb0348cb183d413bb34318b8243c97dc79b412
Jamie Gaehring <gaehj457@gmail.com>
2019-02-05 18:37:32 -0500

    create a basic logout screen

---
+++ src/login/Logout.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+
+  <div class="">
+    <h1>Logout</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Logout',
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/login/index.js
+++ src/login/index.js
@@ -1,14 +1,23 @@
 import Login from './Login.vue'; // eslint-disable-line import/extensions
+import Logout from './Logout.vue'; // eslint-disable-line import/extensions
 import loginModule from './loginModule';
 
 export default {
   install(Vue, { router, store }) {
     const LoginComponent = Vue.component(Login.name, Login);
-    router.addRoutes([{
-      path: '/login',
-      name: 'Login',
-      component: LoginComponent,
-    }]);
+    const LogoutComponent = Vue.component(Logout.name, Logout);
+    router.addRoutes([
+      {
+        path: '/login',
+        name: 'Login',
+        component: LoginComponent,
+      },
+      {
+        path: '/logout',
+        name: 'Logout',
+        component: LogoutComponent,
+      },
+    ]);
     store.registerModule('auth', loginModule);
   },
 };
