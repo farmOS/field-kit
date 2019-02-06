7bf840ffb8ddae4cac8f2f96cd8e78d088f36391
Jamie Gaehring <gaehj457@gmail.com>
2019-02-05 19:22:39 -0500

    add basic markup for logout screen

--- src/login/Logout.vue
+++ src/login/Logout.vue
@@ -1,16 +1,47 @@
 <template lang="html">
+  <div class="container-fluid">
 
-  <div class="">
-    <h1>Logout</h1>
+    <div id="first-row" class="row">
+      <div class="col">
+        <div class="card text-primary border-primary mb-3">
+          <div class="card-body">
+            <h4 class="card-title">WARNING!</h4>
+            <p class="card-text">
+              Logging out will permanently delete all logs stored on this device.
+              You will not be able to recover logs which have not been synced with
+              your farmOS server. Go back to sync any unsaved logs, or proceed to
+              logout and clear all data from this device.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-sm-center">
+      <div id="go-back" class="col col-sm-auto">
+        <button class="btn btn-lg btn-success" type="button" name="button">Go Back</button>
+      </div>
+      <div class="col col-sm-auto">
+        <button class="btn btn-lg btn-primary" type="button" name="button">Logout</button>
+      </div>
+    </div>
+
+
   </div>
-
 </template>
 
 <script>
-export default {
-  name: 'Logout',
-}
+  export default {
+    name: 'Logout',
+  }
 </script>
 
 <style lang="css" scoped>
+  #first-row {
+    margin-top: 3rem;
+  }
+
+  #go-back {
+    text-align: right;
+  }
 </style>
