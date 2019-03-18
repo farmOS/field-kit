4347346277ae02bbe8c07400734f4acaf61e5288
Jamie Gaehring <gaehj457@gmail.com>
2019-03-18 16:28:27 -0400

    upgrade Vuex and refactor to use 'after' subscribe handlers

--- src/data/index.js
+++ src/data/index.js
@@ -99,71 +99,73 @@ export default {
         localStorage.setItem('useGeolocation', mutation.payload);
       }
     });
-    store.subscribeAction((action) => {
-      if (action.type === 'forceSyncAssetsAndAreas') {
-        if (localStorage.getItem('host') !== null) {
-          store.dispatch('updateAssets').then().catch((err) => {
-            if (err.status === 403 || err.status === 401) {
-              router.push('/login');
-              return;
-            }
-            const errorPayload = {
-              message: `${err.status} error while syncing assets: ${err.statusText}`,
-              errorCode: err.statusText,
-              level: 'warning',
-              show: true,
-            };
-            store.commit('logError', errorPayload);
-          });
-          store.dispatch('updateAreas').then().catch((err) => {
-            if (err.status === 403 || err.status === 401) {
-              router.push('/login');
-              return;
-            }
-            const errorPayload = {
-              message: `${err.status} error while syncing areas: ${err.statusText}`,
-              errorCode: err.statusText,
-              level: 'warning',
-              show: true,
-            };
-            store.commit('logError', errorPayload);
-          });
-          return;
+    store.subscribeAction({
+      after: (action) => {
+        if (action.type === 'forceSyncAssetsAndAreas') {
+          if (localStorage.getItem('host') !== null) {
+            store.dispatch('updateAssets').then().catch((err) => {
+              if (err.status === 403 || err.status === 401) {
+                router.push('/login');
+                return;
+              }
+              const errorPayload = {
+                message: `${err.status} error while syncing assets: ${err.statusText}`,
+                errorCode: err.statusText,
+                level: 'warning',
+                show: true,
+              };
+              store.commit('logError', errorPayload);
+            });
+            store.dispatch('updateAreas').then().catch((err) => {
+              if (err.status === 403 || err.status === 401) {
+                router.push('/login');
+                return;
+              }
+              const errorPayload = {
+                message: `${err.status} error while syncing areas: ${err.statusText}`,
+                errorCode: err.statusText,
+                level: 'warning',
+                show: true,
+              };
+              store.commit('logError', errorPayload);
+            });
+            return;
+          }
+          router.push('/login');
         }
-        router.push('/login');
-      }
-      if (action.type === 'getLogs') {
-        /*
-        Triggered when the 'sync' button is pressed in allLogs
-        First set all local logs ready to sync. This status will be retained unless
-        the local log is over-written with a log from the server.
-        */
-        store.commit('updateAllLogs', logSyncer);
-        // Get and process logs from the server in httpModule
-        store.dispatch('getServerLogs')
-          .then(() => {
-            // Save the current time as the most recent syncDate
-            localStorage.setItem('syncDate', (Date.now() / 1000).toFixed(0));
-            // After getServerLogs finishes, we send logs with isReadyToSync true to the server
-            const indices = store.state.farm.logs.reduce(syncReducer, []);
-            store.dispatch('sendLogs', { indices, router });
-          }).catch((err) => {
-            if (err.status === 403 || err.status === 401) {
-              router.push('/login');
-              return;
-            }
-            const errorPayload = {
-              message: `${err.status} error while syncing logs: ${err.statusText}`,
-              errorCode: err.statusText,
-              level: 'warning',
-              show: true,
-            };
-            store.commit('logError', errorPayload);
-          });
-      }
-      if (action.type === 'serverLogToDb') {
-        store.dispatch('createLogFromServer', action.payload);
-      }
+        if (action.type === 'getLogs') {
+          /*
+          Triggered when the 'sync' button is pressed in allLogs
+          First set all local logs ready to sync. This status will be retained unless
+          the local log is over-written with a log from the server.
+          */
+          store.commit('updateAllLogs', logSyncer);
+          // Get and process logs from the server in httpModule
+          store.dispatch('getServerLogs')
+            .then(() => {
+              // Save the current time as the most recent syncDate
+              localStorage.setItem('syncDate', (Date.now() / 1000).toFixed(0));
+              // After getServerLogs finishes, we send logs with isReadyToSync true to the server
+              const indices = store.state.farm.logs.reduce(syncReducer, []);
+              store.dispatch('sendLogs', { indices, router });
+            }).catch((err) => {
+              if (err.status === 403 || err.status === 401) {
+                router.push('/login');
+                return;
+              }
+              const errorPayload = {
+                message: `${err.status} error while syncing logs: ${err.statusText}`,
+                errorCode: err.statusText,
+                level: 'warning',
+                show: true,
+              };
+              store.commit('logError', errorPayload);
+            });
+        }
+        if (action.type === 'serverLogToDb') {
+          store.dispatch('createLogFromServer', action.payload);
+        }
+      },
     });
   },
 };
