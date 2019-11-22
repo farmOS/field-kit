2f1015cdef4b902932d96ddfd3a965a1bce23148
Jamie Gaehring <gaehj457@gmail.com>
2019-11-22 09:53:08 -0500

    Revert "remove test plugin"
    This reverts commit 70f337519ead7a3407d6e1d30c2aa3b7b57a4733.

--- src/core/app.js
+++ src/core/app.js
@@ -4,14 +4,41 @@ import store from './store';
 import App from './App.vue'; // eslint-disable-line import/extensions
 import './bootstrap-simplex.min.css';
 import './vars.css';
+import manifest from '../field-modules/test-plugin/manifest.json';
 
 Vue.config.productionTip = false;
+
+// Mock the farmOS.js library for now
+const fakeFarm = {
+  info() {
+    return Promise.resolve({
+      client: {
+        modules: [manifest],
+      },
+    });
+  },
+};
 
 export default (el, plugins) => {
   // Load build-time plugins
   if (plugins !== undefined && plugins.length > 0) {
     plugins.forEach(p => Vue.use(p, { store, router }));
   }
+  // Fetch and load field modules at runtime
+  fakeFarm.info()
+    .then(res => res.client.modules.forEach((module) => {
+      const script = document.createElement('script');
+      script.src = module.index;
+      script.id = `field-module-${module.name}`;
+      script.type = 'module';
+      script.async = true;
+      script.onload = () => Vue.use(
+        { install: window[`install${module.name}`] },
+        { store, router },
+      );
+      script.onerror = () => console.error(`Error installing ${module.name} module`);
+      document.body.appendChild(script);
+    }));
   return new Vue({
     el,
     store,
---
+++ src/field-modules/test-plugin/components/TestComponentAMenuBar.js
@@ -0,0 +1,4 @@
+export default {
+  name: 'TestAMenu',
+  template: '<nav>TestComponentAMenuBar</nav>',
+};
