36b1e0c44d1d787a475a950ce5728416cffd123c
Alex Smith <alexasbeet@gmail.com>
2020-01-25 18:49:36 -0500

    adds click behavior and styles homescreen

---
+++ src/field-modules/test-plugin/components/Widget.vue
@@ -0,0 +1,24 @@
+<template>
+  <widget>
+    <p> Test Component Widget </p>
+  </widget>
+</template>
+
+<script>
+import Widget from '@/components/Widget';
+
+export default {
+  components: {
+    Widget,
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+  a, a:hover {
+    text-decoration: none;
+    color: inherit;
+  }
+</style>
