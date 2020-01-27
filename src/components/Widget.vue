bdae49ffb4ffb63d39b24c1d1c7d676292f48be3
Alex Smith <alexasbeet@gmail.com>
2020-01-26 20:03:57 -0500

    Tiles widgets in homescreen

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,49 +1,22 @@
 <template functional>
+
   <div class="widget">
-    <div class="card">
-      <div class="card-body">
-        <slot></slot>
-      </div>
+    <div class="box">
+      <slot></slot>
     </div>
   </div>
+
 </template>
 
 <style scoped>
 
-  .card {
-    max-width: 18rem;
-  }
-
-  .card-group > .card {
-    margin-bottom: 0;
-  }
-
-  .card-body {
-    color: var(--gray-dark);
-  }
-
-  .card-row-1 {
-    display: flex;
-    flex-flow: row nowrap;
-  }
-
-  .card-row-1 h5 {
-    font-weight: 700;
-  }
-
-  .card-row-2 {
-    display: flex;
-    flex-flow: row nowrap;
-  }
-
-  .card-row-3 {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-  }
-
-  .card-body p {
-    margin-bottom: 0.5rem;
-  }
+.box {
+    margin-bottom: 20px;
+    height:150px;
+    background: #ffffff;
+    border: 1px solid rgba(2, 154, 207, 1);
+    border-radius: 10px;
+    padding: 20px;
+}
 
 </style>
