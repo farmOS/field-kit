0bd623d91ada753b83bafdf23a76fc15962e526f
Jamie Gaehring <gaehj457@gmail.com>
2020-01-29 19:33:38 -0500

    Remove conditional class bindings.

--- src/field-modules/my-logs/components/Widget.vue
+++ src/field-modules/my-logs/components/Widget.vue
@@ -4,13 +4,13 @@
     <h4 class="card-title">My Logs Widget</h4>
     <h5
       @click="openMyLogs"
-      :class="{ clickable: true }">
+      class="clickable">
       All Logs
       <IconAssignment class="inline-svg"/>
     </h5>
     <h5
       @click="openFilter"
-      :class="{ clickable: true }">
+      class="clickable">
       Filter Logs
       <IconFilter class="inline-svg"/>
     </h5>
