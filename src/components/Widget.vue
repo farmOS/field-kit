d8b63d53fa4f00a82d5de0e8b119abb0a0739673
Jamie Gaehring <gaehj457@gmail.com>
2020-02-17 23:39:50 -0500

    Style the main grid for the home screen.

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,22 +1,11 @@
 <template functional>
 
-  <div class="widget">
-    <div class="box">
-      <slot></slot>
-    </div>
+  <div>
+    <slot></slot>
   </div>
 
 </template>
 
 <style scoped>
-
-.box {
-    margin-bottom: 20px;
-    height:150px;
-    background: #ffffff;
-    border: 1px solid rgba(2, 154, 207, 1);
-    border-radius: 10px;
-    padding: 20px;
-}
 
 </style>
--- src/core/homescreen/Homescreen.vue
+++ src/core/homescreen/Homescreen.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class=body>
-    <div id="container" class="cols">
-      <homescreen-widgets :modules="modules"/>
-    </div>
+  <div id="homescreen">
+    <homescreen-widgets :modules="modules"/>
   </div>
 </template>
 
@@ -13,18 +11,38 @@ import { mapState } from 'vuex';
 const HomescreenWidgets = Vue.component('homescreen-widgets', { // eslint-disable-line no-unused-vars
   render(createElement) {
     const self = this;
-    return createElement('div', this.modules.map(module => createElement(
-      `${module.name}-widget`,
+    return createElement(
+      'div',
       {
-        nativeOn: {
-          click() {
-            self.$store.commit('setCurrentModule', module.name);
-            self.$store.commit('filterLogs', log => log.modules.includes(module.name));
-            self.$store.dispatch('loadCachedLogs');
-          },
+        style: {
+          display: 'flex',
+          flexFlow: 'row wrap',
+          maxWidth: '700px',
+          minHeight: 'calc(100vh - 3rem)',
+          margin: 'auto',
+          backgroundColor: '#fff',
         },
       },
-    )));
+      this.modules.map(module => createElement(
+        `${module.name}-widget`,
+        {
+          style: {
+            flex: '0 0 50%',
+            height: '50vw',
+            maxHeight: '350px',
+            backgroundColor: '#fff',
+            border: '1px solid #eee',
+          },
+          nativeOn: {
+            click() {
+              self.$store.commit('setCurrentModule', module.name);
+              self.$store.commit('filterLogs', log => log.modules.includes(module.name));
+              self.$store.dispatch('loadCachedLogs');
+            },
+          },
+        },
+      )),
+    );
   },
   props: {
     modules: {
@@ -43,20 +61,7 @@ export default {
 </script>
 
 <style scoped>
-  #container {
-      width: 100%;
-      max-width: 700px;
-      margin: 2em auto;
-  }
-  .cols {
-      -moz-column-count:2;
-      -moz-column-gap: 2%;
-      -moz-column-width: 48%;
-      -webkit-column-count:2;
-      -webkit-column-gap: 2%;
-      -webkit-column-width: 48%;
-      column-count: 2;
-      column-gap: 2%;
-      column-width: 48%;
+  #homescreen {
+    background-color: #eee;
   }
 </style>
