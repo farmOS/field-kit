18887f3f73f4f422e89916af2913f1bf71b89b39
Jamie Gaehring <gaehj457@gmail.com>
2020-03-17 19:34:12 -0400

    Remove weather module from this repo.

--- src/field-modules/weather/index.js
+++
@@ -1,7 +0,0 @@
-import modConfig from './module.config';
-
-const { createFieldModule } = window.farmOS.utils;
-
-const WeatherFieldModule = createFieldModule(modConfig);
-
-export default WeatherFieldModule;
