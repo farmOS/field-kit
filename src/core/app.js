eaee50ed68700c6fbb647b2f08e5acc8cb191a99
Jamie Gaehring <gaehj457@gmail.com>
2020-03-17 19:26:59 -0400

    Load field modules from a live server.

--- src/core/app.js
+++ src/core/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import farmConnect from 'farmos';
 import router from './router';
 import store from './store';
 import App from './App.vue'; // eslint-disable-line import/extensions
@@ -9,22 +10,16 @@ import components from '../components';
 
 Vue.config.productionTip = false;
 
-// Mock the farmOS.js library for now
-const fakeFarm = {
-  info() {
-    return Promise.resolve({
-      client: {
-        modules: [manifest],
-      },
-    });
-  },
-};
-
 // Attach utils to the global namespace so Field Modules can access them.
 if (window.farmOS === undefined) {
   window.farmOS = {};
 }
 window.farmOS.utils = utils;
+
+// Provide a global namespace to which modules can attach their config.
+if (window.farmOS.modules === undefined) {
+  window.farmOS.modules = {};
+}
 
 // Register the shared component library globally so they can be accessed from
 // any other component on the root Vue instance.
@@ -35,26 +30,35 @@ export default (el, plugins) => {
   if (plugins !== undefined && plugins.length > 0) {
     plugins.forEach(p => Vue.use(p, { store, router }));
   }
+
   // Fetch and load field modules at runtime
-  fakeFarm.info()
-    .then(res => res.client.modules.forEach((module) => {
-      const script = document.createElement('script');
-      script.src = module.index;
-      script.id = `field-module-${module.name}`;
-      script.type = 'module';
-      script.async = true;
-      script.onload = () => Vue.use(
-        { install: window[`install${module.name}`] },
-        { store, router },
-      );
-      script.onerror = () => console.error(`Error installing ${module.name} module`);
-      document.body.appendChild(script);
+  const farm = farmConnect(
+    localStorage.getItem('host'),
+    localStorage.getItem('username'),
+    localStorage.getItem('password'),
+  );
+  farm.info()
+    .then((res) => {
+      Object.values(res.client.modules).forEach((module) => {
+        const script = document.createElement('script');
+        script.src = module.js;
+        script.id = `field-module-${module.name}`;
+        script.type = 'module';
+        script.async = true;
+        script.onload = () => {
+          const config = window.farmOS.modules[module.name];
+          const plugin = utils.createFieldModule(config);
+          Vue.use(plugin, { store, router });
+        };
+        script.onerror = () => console.error(`Error installing ${module.label} module`);
+        document.body.appendChild(script);
+      });
+    })
+    .finally(() => new Vue({
+      el,
+      store,
+      router,
+      components: { App },
+      template: '<App/>',
     }));
-  return new Vue({
-    el,
-    store,
-    router,
-    components: { App },
-    template: '<App/>',
-  });
 };
