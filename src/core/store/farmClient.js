23bfe4ffd1ab6003725b3d4bb24e2700894934b9
Jamie Gaehring <gaehj457@gmail.com>
2020-07-27 14:04:03 -0400

    Check for CSRF token in localStorage.

--- src/core/store/farmClient.js
+++ src/core/store/farmClient.js
@@ -1,5 +1,16 @@
 import farmOS from 'farmos';
 
+// For pre-0.5.0 versions, make sure there's not a CSRF token left in localStorage.
+try {
+  const token = JSON.parse(localStorage.getItem('token'));
+  if (typeof token !== 'object' && token !== null) {
+    throw new Error('Invalid token in localStorage.');
+  }
+} catch (e) {
+  console.error(e);
+  localStorage.removeItem('token');
+}
+
 let host = localStorage.getItem('host');
 const getToken = () => JSON.parse(localStorage.getItem('token'));
 const setToken = token => localStorage.setItem('token', JSON.stringify(token));
