d8b0ecb1e24587628a41a0eae83f2f773462a943
rafaeltcc <BMW+2XLtNaGL+CJHlR8=>
2020-08-06 18:15:38 -0300

    First translations in pt.js

--- src/core/store/l10n/translations/pt.js
+++ src/core/store/l10n/translations/pt.js
@@ -4,15 +4,22 @@ export default {
   strings: {
     // DIRECTORY: core/
     // App.vue
+    'Welcome to farmOS': 'Bem-vindo ao farmOs',
+    'Login for more options.':'Login para mais opções',
+    'Home': 'Início',
+    'Share My Location':'Compartilhar minha localização',
     'Select Language': 'Selecionar o Idioma',
     'English': 'Inglês',
     'Portuguese': 'Portugues',
+    'Login':'Entrar',
+    'Logout':'Sair',
     // Home.vue
     // HomeMenuBar.vue
     // Login.vue
     // Logout.vue
     // DIRECTORY: field-modules/my-logs/
     // AllLogs.vue
+    
     // AllLogsMenuBar.vue
     // etc...
   },
