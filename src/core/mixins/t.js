03a286b23433f7969340ce8f87ad334f99316e3e
Jamie Gaehring <gaehj457@gmail.com>
2020-08-16 20:46:35 -0400

    Force update all components when locale changes.

--- src/core/mixins/t.js
+++ src/core/mixins/t.js
@@ -1,6 +1,15 @@
+import { mapState } from 'vuex';
 import { $t } from '../store/l10n/t';
 
 // A global Vue mixin for translating strings in the markup.
 export default {
+  computed: mapState({
+    locale: state => state.l10n.locale,
+  }),
   methods: { $t },
+  watch: {
+    locale() {
+      this.$forceUpdate();
+    },
+  },
 };
