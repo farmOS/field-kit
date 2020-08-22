366cb92bf455d289d79ba8c3d2fb77d37fcf912a
Jamie Gaehring <gaehj457@gmail.com>
2020-08-22 12:59:58 -0400

    Mock localStorage.

--- test-setup.js
+++ test-setup.js
@@ -1,3 +1,23 @@
 require('jsdom-global')();
 
 global.expect = require('expect');
+
+const makeLocalStorage = () => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, val) {
+      store[key] = val;
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+}
+
+global.localStorage = makeLocalStorage();
