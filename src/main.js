b6c8ad9eda7cf7e53ba68a67688ec9a80679fdc3
Jamie Gaehring <gaehj457@gmail.com>
2020-09-03 15:56:59 -0400

    Prompt refresh when update found w/ confirm().

--- src/main.js
+++ src/main.js
@@ -5,10 +5,25 @@ app('#app', [
   logs,
 ]);
 
+const onUpdateFound = registration => () => {
+  registration.installing.addEventListener('statechange', (e) => {
+    const msg = 'A new version of Field Kit is available. '
+    + 'Click OK to load it now, or Cancel to go back to the app.';
+    // eslint-disable-next-line no-alert
+    if (e.target.state === 'installed' && window.confirm(msg)) {
+      navigator.serviceWorker.ready
+        .then(reg => reg.active.postMessage({ type: 'SKIP_WAITING' }));
+    }
+  });
+};
+
 // Check that service workers are registered (for production environment only)
 if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        registration.addEventListener('updatefound', onUpdateFound(registration));
+      });
   });
 }
