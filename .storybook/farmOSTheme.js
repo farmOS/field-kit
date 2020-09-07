9b59e28aa69d51a1cf451e52ef80b804ae56ec9f
Jamie Gaehring <gaehj457@gmail.com>
2020-09-07 16:04:13 -0400

    Add farmOSTheme to Storybook.

---
+++ .storybook/farmOSTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'farmOS Field Kit Storybook',
+  brandUrl: 'https://farmos.org',
+  brandImage: 'https://farmos.org/img/farmOS.png',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import farmOSTheme from './farmOSTheme';
+
+addons.setConfig({
+  theme: farmOSTheme,
+});
