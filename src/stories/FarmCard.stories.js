f006aece7fff26d28f98cc78e7f498ee53605e8d
Jamie Gaehring <gaehj457@gmail.com>
2020-09-08 09:15:37 -0400

    Rewrite FarmCard stories.

--- src/stories/FarmCard.stories.js
+++ src/stories/FarmCard.stories.js
@@ -1,24 +1,43 @@
 import FarmCard from '../components/FarmCard';
+import FarmStack from '../components/FarmStack';
+import FarmInline from '../components/FarmInline';
 
 export default {
   title: 'Content/FarmCard',
   component: FarmCard,
+  subcomponents: { FarmStack, FarmInline },
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { FarmCard },
-  template: args.template || '<farm-card><p>Body text</p></farm-card>',
+  template: `
+    <farm-card :space="space" :width="width" :height="height" :breakpoints="breakpoints" :boxShadow="boxShadow" :backgroundColor="backgroundColor">
+      <farm-stack>
+        <farm-inline><h4>Title</h4></farm-inline>
+        <farm-inline><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></farm-inline>
+        <farm-inline>
+          <farm-chip>Tomatoes</farm-chip>
+          <farm-chip color="green">Broccoli</farm-chip>
+        </farm-inline>
+      </farm-stack>
+    </farm-card>
+  `,
 });
 
 export const Basic = Template.bind({});
 
-export const WithTitle = Template.bind({});
-WithTitle.args = {
-  template: '<farm-card><farm-inline><h4>Title</h4></farm-inline></farm-card>',
+export const withMoreSpace = Template.bind({});
+withMoreSpace.args = {
+  space: '3rem',
 };
 
-export const WithChips = Template.bind({});
-WithChips.args = {
-  template: '<farm-card><farm-stack><farm-inline><h4>Title</h4></farm-inline><farm-inline><farm-chip>Tomatoes</farm-chip><farm-chip color="green">Broccoli</farm-chip></farm-inline></farm-stack></farm-card>',
+export const withWidth = Template.bind({});
+withWidth.args = {
+  width: '400px',
+};
+
+export const withResponsiveWidth = Template.bind({});
+withResponsiveWidth.args = {
+  width: ['200px', '400px', '600px'],
 };
