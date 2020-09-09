be28ae5562f7175ada8e7a89c1d1ab916cdc8920
Jamie Gaehring <gaehj457@gmail.com>
2020-09-09 13:54:21 -0400

    Add story for all icons.

---
+++ src/stories/Icons.stories.js
@@ -0,0 +1,29 @@
+import IconsTemplate from './IconsTemplate';
+
+export default {
+  title: 'Icons/All',
+  component: IconsTemplate,
+  argTypes: {
+    height: {
+      control: {
+        type: 'range',
+        step: 12,
+        min: 12,
+        max: 72,
+      },
+    },
+    fill: { control: 'color' },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { IconsTemplate },
+  template: '<icons-template :height="height" :fill="fill"/>',
+});
+
+export const Icons = Template.bind({});
+Icons.args = {
+  height: 60,
+  fill: 'black',
+};
---
+++ src/stories/IconsTemplate.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <farm-stack space="1rem">
+      <farm-card>
+        <farm-stack>
+          <h1>farmOS Field Kit Icons</h1>
+          <p>
+            The icon set for farmOS Field Kit is comprised mainly of icons from
+            the <a href="https://material.io/tools/icons/" target="_blank">
+            Material Design Icon Set</a>. They've been wrapped as Vue components
+            and registered globally so they can be accessed by any module. Our
+            naming convention prefixes them with `icon-`, so for example, if you
+            wanted to use the "Add Circle" icon, you'd include it in your
+            tempate as <code>&lt;icon-add-circle/&gt;</code>.
+          </p>
+          <p>
+            The props for any icon include <code>width</code>, <code>height</code>,
+            and <code>fill</code>. The default heigh and width is <code>24</code>,
+            which is in pixels; however, for this demo, only the height is
+            adjustable, with the width pinned to the value of the height, and
+            the default is set to <code>60</code> for greater visibility.
+          </p>
+        </farm-stack>
+      </farm-card>
+      <icon-tiles :height="height" :fill="fill" />
+    </farm-stack>
+  </div>
+</template>
+
+<script>
+import components from '../components';
+
+function convertPascalToKebab(name) {
+  return name
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
+    .replace(/^[^a-zA-Z]+/g, '')
+    .toLowerCase();
+}
+
+function convertNameToTitle(name) {
+  return name
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1 $2')
+    .replace(/Icon/g, '')
+    .trim();
+}
+
+function convertNameToTag(name) {
+  return `<${convertPascalToKebab(name)}/>`;
+}
+
+const IconTiles = {
+  name: 'IconTiles',
+  props: {
+    height: {
+      type: Number,
+      default: 60,
+    },
+    fill: {
+      type: String,
+      default: 'black',
+    },
+  },
+  render(h) {
+    return h(
+      'farm-tiles',
+      components
+        .filter(c => c.name.startsWith('Icon'))
+        .map(icon => h(
+          'farm-card',
+          [h(
+            'farm-stack',
+            [
+              h(
+                'div',
+                { style: { textAlign: 'center' } },
+                [h(icon, { props: { width: this.height, height: this.height, fill: this.fill } })],
+              ),
+              h('h4', { style: { textAlign: 'center' } }, convertNameToTitle(icon.name)),
+              h('pre', { style: { textAlign: 'center' } }, convertNameToTag(icon.name)),
+            ],
+          )],
+        )),
+    );
+  },
+};
+
+export default {
+  name: 'IconsTemplate',
+  props: {
+    height: {
+      type: Number,
+      default: 60,
+    },
+    fill: {
+      type: String,
+      default: 'black',
+    },
+  },
+  components: { IconTiles },
+};
+</script>
+
+<style>
+
+</style>
