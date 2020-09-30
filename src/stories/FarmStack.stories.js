e10443681312e993a029ae073635a9f330a43719
Jamie Gaehring <gaehj457@gmail.com>
2020-09-30 10:13:47 -0400

    Add FarmStack story.

---
+++ src/stories/FarmStack.stories.js
@@ -0,0 +1,65 @@
+import FarmStack from '../components/FarmStack';
+import FarmPlaceholder from './FarmPlaceholder';
+
+export default {
+  title: 'Layout/FarmStack',
+  component: FarmStack,
+  argTypes: {
+    space: {
+      control: {
+        type: 'select',
+        options: [
+          'xxxs', 'xxs', 'xs', 's',
+          'm', 'l', 'xl', 'xxl', 'none',
+        ],
+      },
+    },
+    dividers: {
+      control: {
+        type: 'select',
+        options: [
+          'regular', 'strong', true, false,
+        ],
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FarmStack, FarmPlaceholder },
+  template: `
+    <farm-stack :space="space" :dividers="dividers">
+      <farm-placeholder/>
+      <farm-placeholder height="150px" width="150px"/>
+      <farm-placeholder/>
+      <farm-placeholder height="150px" width="150px"/>
+      <farm-placeholder/>
+      <farm-placeholder height="150px" width="150px"/>
+    </farm-stack>
+  `,
+});
+
+export const Default = Template.bind({});
+
+export const withSpace = Template.bind({});
+withSpace.args = {
+  space: 'm',
+};
+
+export const withMoreSpace = Template.bind({});
+withMoreSpace.args = {
+  space: 'xl',
+};
+
+export const withDividers = Template.bind({});
+withDividers.args = {
+  space: 'm',
+  dividers: true,
+};
+
+export const withStrongDividers = Template.bind({});
+withStrongDividers.args = {
+  space: 'm',
+  dividers: 'strong',
+};
