992c51098e125688c2c5f16cab67709f7b09b10a
Jamie Gaehring <gaehj457@gmail.com>
2020-09-30 21:02:42 -0400

    Add FarmDivider story.

---
+++ src/stories/FarmDivider.stories.js
@@ -0,0 +1,31 @@
+import FarmDivider from '../components/FarmDivider';
+
+export default {
+  title: 'Layout/FarmDivider',
+  component: FarmDivider,
+  argTypes: {
+    weight: {
+      control: {
+        type: 'select',
+        options: [
+          'regular', 'strong',
+        ],
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FarmDivider },
+  template: `
+    <farm-divider :weight="weight"/>
+  `,
+});
+
+export const Default = Template.bind({});
+
+export const Strong = Template.bind({});
+Strong.args = {
+  weight: 'strong',
+};
