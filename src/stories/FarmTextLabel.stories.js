810b171b5e6a63150f5f221dccd6a44311d6e618
Jamie Gaehring <gaehj457@gmail.com>
2020-09-30 21:24:17 -0400

    Add FarmTextLabel story.

---
+++ src/stories/FarmTextLabel.stories.js
@@ -0,0 +1,60 @@
+import FarmTextLabel from '../components/FarmTextLabel';
+
+export default {
+  title: 'Content/FarmTextLabel',
+  component: FarmTextLabel,
+  argTypes: {
+    as: {
+      control: {
+        type: 'select',
+        options: [
+          'p', 'div', 'span', 'li', 'pre', 'label', 'input',
+        ],
+      },
+    },
+    color: {
+      control: {
+        type: 'select',
+        options: [
+          'text', 'dark', 'subtle', 'white',
+          'primary', 'secondary', 'tertiary',
+        ],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: [
+          's', 'm', 'l', 'xl',
+        ],
+      },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FarmTextLabel },
+  template: `
+    <farm-text-label :as="as" :color="color" :size="size">
+      Label
+    </farm-text-label>
+  `,
+});
+
+export const Default = Template.bind({});
+
+export const asPTag = Template.bind({});
+asPTag.args = {
+  as: 'p',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 's',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'l',
+};
