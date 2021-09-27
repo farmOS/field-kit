170eb3a8ec998eabe1450c8dfe410437ac6e0211
Jamie Gaehring <gaehj457@gmail.com>
2021-09-27 13:57:49 -0400

    Check for any nullish value in parseNotes.

--- src/core/utils/parseNotes.js
+++ src/core/utils/parseNotes.js
@@ -1,5 +1,5 @@
 export default function parseNotes(notes) {
-  if (notes === null) { return ''; }
+  if (!notes) { return ''; }
   const tmp = document.createElement('div');
   tmp.innerHTML = notes.value;
   return tmp.textContent || tmp.innerText || '';
