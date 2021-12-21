5b901ce1bd688710964c15e14d7af5580f52d5e4
Jamie Gaehring <gaehj457@gmail.com>
2021-12-21 15:28:59 -0500

    Update farmOS.js to 2.0.0-beta.2.

--- src/core/farm.js
+++ src/core/farm.js
@@ -1,22 +1,19 @@
 import farmOS from 'farmos';
-import schemata from 'farmos/src/model/schemata/stub';
 
 export const getHost = () => localStorage.getItem('host');
 
 const remote = {
-  options: {
-    host: getHost(),
-    clientId: 'farm_client',
-    getToken: () => JSON.parse(localStorage.getItem('token')),
-    setToken: token => localStorage.setItem('token', JSON.stringify(token)),
-  },
+  host: getHost() || '',
+  clientId: 'fieldkit',
+  getToken: () => JSON.parse(localStorage.getItem('token')),
+  setToken: token => localStorage.setItem('token', JSON.stringify(token)),
 };
 
-const farm = farmOS({ schemata, remote });
+const farm = farmOS({ remote });
 
 export default farm;
 
 export const setHost = (host) => {
+  farm.remote.add({ host, ...remote });
   localStorage.setItem('host', host);
-  farm.remote.setHost(host);
 };
