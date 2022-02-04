6dae63b3d6b94e01b648cd03ba090851cc6cbc34
Jamie Gaehring <gaehj457@gmail.com>
2022-02-04 14:16:36 -0500

    Use Material (Google Fonts) filter icon.

--- src/components/icons/icon-filter.vue
+++ src/components/icons/icon-filter.vue
@@ -1,9 +1,5 @@
 <template>
-  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" :width="width" :height="height" :fill="fill" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-    <polygon points="222.949,302.964 222.949,512 289.051,467.933 289.051,302.964"/>
-    <rect x="2.612" width="506.776" height="49.576"/>
-    <polygon points="19.137,82.627 206.424,269.913 305.576,269.913 492.863,82.627"/>
-  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" :width="24" :height="24" :fill="fill"><g><path d="M0,0h24 M24,24H0" :fill="fill"/><path d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"/></g></svg>
 </template>
 
 <script>
