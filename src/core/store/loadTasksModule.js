25f9420d879b7f7b31fd844e70bda67cc0599933
Jamie Gaehring <gaehj457@gmail.com>
2022-02-23 19:08:14 -0500

    Dynamically import tasks module from pkg dist.

--- src/core/store/loadTasksModule.js
+++ src/core/store/loadTasksModule.js
@@ -1,13 +1,4 @@
 const loadTasksModule = () =>
-  import('../../../packages/field-modules/tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0')
-    .then(({ default: tasks }) => {
-      window.farmOS.mountFieldModule(tasks);
-    })
-    .catch((e) => {
-      const msg = 'Error loading Tasks Module.\n';
-      // eslint-disable-next-line no-console
-      if (process.env.NODE_ENV === 'development') console.error(msg, e);
-      throw e;
-    });
+  import('../../../packages/field-modules/tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0');
 
 export default loadTasksModule;
