e44f001c67d194affd979a311453e2d5b345e209
Jamie Gaehring <gaehj457@gmail.com>
2022-02-23 19:25:44 -0500

    Move field-modules/tasks to field-module-tasks.

--- src/core/store/loadTasksModule.js
+++ src/core/store/loadTasksModule.js
@@ -1,4 +1,4 @@
 const loadTasksModule = () =>
-  import('../../../packages/field-modules/tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0');
+  import('../../../packages/field-module-tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0');
 
 export default loadTasksModule;
