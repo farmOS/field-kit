eb7a088f64ae04daa3937b850974ccefa2115366
Jamie Gaehring <gaehj457@gmail.com>
2022-02-24 20:53:29 -0500

    Catch & resolve failed calls to loadTasksModule.

--- src/core/store/loadTasksModule.js
+++ src/core/store/loadTasksModule.js
@@ -1,4 +1,11 @@
 const loadTasksModule = () =>
-  import('../../../packages/field-module-tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0');
+  import('../../../packages/field-module-tasks/dist/farm_fieldkit_tasks/js/tasks.1-0-0')
+    .catch((e) => {
+      if (process.env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.log('Tasks module failed to load. Error: ', e);
+      }
+      return Promise.resolve();
+    });
 
 export default loadTasksModule;
