32f9de92f4d0c2d5f81e9e59cd5e9bf9ba8abbcf
Jamie Gaehring <gaehj457@gmail.com>
2022-03-10 21:11:29 -0500

    Import entity nomenclature from farmOS.js.

--- src/core/store/nomenclature.js
+++ src/core/store/nomenclature.js
@@ -1,54 +1,10 @@
+import { entities } from 'farmos';
+
 const nomenclature = {
-  entities: {
-    asset: {
-      name: 'asset',
-      shortName: 'asset',
-      plural: 'assets',
-      shortPlural: 'assets',
-      display: 'Asset',
-      displayPlural: 'Assets',
-    },
-    log: {
-      name: 'log',
-      shortName: 'log',
-      plural: 'logs',
-      shortPlural: 'logs',
-      display: 'Log',
-      displayPlural: 'Logs',
-    },
-    plan: {
-      name: 'plan',
-      shortName: 'plan',
-      plural: 'plans',
-      shortPlural: 'plans',
-      display: 'Plan',
-      displayPlural: 'Plans',
-    },
-    quantity: {
-      name: 'quantity',
-      shortName: 'quantity',
-      plural: 'quantities',
-      shortPlural: 'quantities',
-      display: 'Quantity',
-      displayPlural: 'Quantities',
-    },
-    taxonomy_term: {
-      name: 'taxonomy_term',
-      shortName: 'term',
-      plural: 'taxonomy_terms',
-      shortPlural: 'terms',
-      display: 'Taxonomy Term',
-      displayPlural: 'Taxonomy Terms',
-    },
-    user: {
-      name: 'user',
-      shortName: 'user',
-      plural: 'users',
-      shortPlural: 'users',
-      display: 'User',
-      displayPlural: 'Users',
-    },
-  },
+  entities: Object.entries(entities).reduce((obj, [name, { nomenclature: n }]) => ({
+    ...obj,
+    [name]: n,
+  }), {}),
 };
 
 export const entityMethods = fn => Object.values(nomenclature.entities)
