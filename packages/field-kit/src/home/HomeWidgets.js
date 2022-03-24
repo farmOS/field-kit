0d92b4d8d72bfb7f23947030ac5021c89a369dfc
Jamie Gaehring <gaehj457@gmail.com>
2022-03-24 11:26:53 -0400

    Handle no modules or widgets in Home screen.

--- packages/field-kit/src/home/HomeWidgets.js
+++ packages/field-kit/src/home/HomeWidgets.js
@@ -1,12 +1,63 @@
+import { is } from 'ramda';
+
+const noModsInfoText = 'It looks like you don\'t have any Field Modules installed, '
+  + 'or they have failed to load. Contact your farmOS server administrator to '
+  + 'install new modules, or troubleshoot why they are not loading.';
+
 const HomeWidgets = {
   name: 'home-widgets',
   props: ['modules'],
-  setup({ modules }) {
+  setup({ modules: allModules }) {
     const { h, resolveComponent } = window.Vue;
     const { useRouter } = window.VueRouter;
     const { component } = window.app;
     const { useEntities } = window.lib;
     const router = useRouter();
+    const widgetModules = allModules.filter(mod => is(Object, mod));
+    const noModulesCard = h(
+      resolveComponent('farm-card'),
+      { space: 's' },
+      {
+        default: () => h(
+          resolveComponent('farm-stack'),
+          { space: 's' },
+          {
+            default: () => [
+              h('h3', null, { default: () => 'Welcome to Field Kit!' }),
+              h(
+                resolveComponent('farm-text'),
+                { size: 's' },
+                { default: () => noModsInfoText },
+              ),
+            ],
+          },
+        ),
+      },
+    );
+    const moduleCards = widgetModules.map((mod) => {
+      const WidgetComponent = component(mod.widget);
+      const onClick = () => { router.push(mod.routes[0].path); };
+      const {
+        append, checkout, commit, revise,
+      } = useEntities({ module: mod });
+      return h(
+        resolveComponent('farm-card'),
+        { onClick },
+        {
+          default() {
+            return [
+              h('h3', mod.label),
+              h(
+                WidgetComponent,
+                {
+                  append, checkout, commit, revise, onClick,
+                },
+              ),
+            ];
+          },
+        },
+      );
+    });
     return () => h(
       resolveComponent('farm-tiles'),
       {
@@ -16,30 +67,10 @@ const HomeWidgets = {
       },
       {
         default() {
-          return modules.map((mod) => {
-            const WidgetComponent = component(mod.widget);
-            const onClick = () => { router.push(mod.routes[0].path); };
-            const {
-              append, checkout, commit, revise,
-            } = useEntities({ module: mod });
-            return h(
-              resolveComponent('farm-card'),
-              { onClick },
-              {
-                default() {
-                  return [
-                    h('h3', mod.label),
-                    h(
-                      WidgetComponent,
-                      {
-                        append, checkout, commit, revise, onClick,
-                      },
-                    ),
-                  ];
-                },
-              },
-            );
-          });
+          if (!Array.isArray(widgetModules) || widgetModules.length < 1) {
+            return [noModulesCard];
+          }
+          return moduleCards;
         },
       },
     );
