f845067701ba9303e3254fc56e3178b1219a3605
Jamie Gaehring <gaehj457@gmail.com>
2023-02-23 22:00:06 -0500

    Accept include option with syncEntities arguments.

--- packages/field-kit/src/http/sync.js
+++ packages/field-kit/src/http/sync.js
@@ -1,19 +1,36 @@
 import {
-  allPass, anyPass, insert, reduce,
+  allPass, anyPass, chain, complement, compose, defaultTo, filter as rFilter,
+  insert, isNil, map, path, prop, reduce,
 } from 'ramda';
 import farm from '../farm';
 import { asFlatArray } from '../utils/asArray';
 import parseFilter from '../utils/parseFilter';
 
-export const fetchEntities = (shortName, { cache, filter, limit }) =>
-  farm[shortName].fetch({ filter, limit }).then((results) => {
+const isNotNil = complement(isNil);
+const getIncludedIds = chain(compose(
+  map(prop('id')),
+  rFilter(isNotNil),
+  defaultTo([]),
+  chain(path(['data', 'included'])),
+));
+function getIncluded(data, fulfilled) {
+  const ids = getIncludedIds(fulfilled);
+  const included = data.filter(d => ids.includes(d.id));
+  return included;
+}
+
+export const fetchEntities = (shortName, { cache, ...rest }) =>
+  farm[shortName].fetch({ ...rest }).then((results) => {
     const { data, fulfilled, rejected } = results;
+    const included = getIncluded(data, fulfilled);
     const entities = data.reduce((collection, remote) => {
       const i = collection.findIndex(ent => ent.id === remote.id);
       const merged = farm[shortName].merge(collection[i], remote);
       return insert(i, merged, collection);
     }, asFlatArray(cache));
-    return { data: entities, fulfilled, rejected };
+    return {
+      data: entities, included, fulfilled, rejected,
+    };
   });
 
 export const syncEntities = (shortName, options) =>
@@ -35,6 +52,7 @@ export const syncEntities = (shortName, options) =>
     const sendWithOptions = e => farm[shortName].send(e, withOptions(e));
     const sendRequests = syncables.map(sendWithOptions);
     const handleSendResults = reduce((result, { status, reason, value: remote }) => {
+      // No need to destructure 'included', since there's no reason to modify it.
       const { data, fulfilled, rejected } = result;
       if (status === 'rejected') {
         return {
@@ -45,9 +63,9 @@ export const syncEntities = (shortName, options) =>
       const i = syncables.findIndex(ent => ent.id === remote.id);
       const merged = farm[shortName].merge(syncables[i], remote);
       return {
+        ...result,
         data: insert(i, merged, data),
         fulfilled: [...fulfilled, remote],
-        rejected,
       };
     }, fetchResults);
     return Promise.allSettled(sendRequests).then(handleSendResults);
