eebf9d167fc1a8939a634436a79f3302a9150617
Jamie Gaehring <gaehj457@gmail.com>
2023-06-24 00:00:55 -0400

    Upgrade Storybook via `npx storybook upgrade` CLI.

--- packages/farm-ui/.storybook/main.js
+++ packages/farm-ui/.storybook/main.js
@@ -1,13 +1,6 @@
 export default {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     "name": "@storybook/vue3-vite",
     "options": {}
